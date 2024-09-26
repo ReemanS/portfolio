a0d7fd03ffe12077ca45eebbedb4ad35b71bff41
Reeman Singh <reemanl.singh422@gmail.com>
2024-09-26 20:14:47 +0800

    add transitions to main section

--- src/app/sections/Main.tsx
+++ src/app/sections/Main.tsx
@@ -3,7 +3,7 @@ import { AiFillMessage } from "react-icons/ai";
 import { HiDocumentDownload } from "react-icons/hi";
 import toast from "react-hot-toast";
 import { iconTheme } from "../constants";
-import { motion } from "framer-motion";
+import { motion, Variant, Variants } from "framer-motion";
 
 export default function Main() {
   const handleComingSoon = () => {
@@ -15,58 +15,104 @@ export default function Main() {
     });
   };
 
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
   const letterVariants = {
-    hidden: { opacity: 0, y: 50 },
+    hidden: { opacity: 0, y: 100 },
     visible: { opacity: 1, y: 0 },
+  };
+
+  const elementVariants: Variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
   };
 
   const name = "Reeman Singh".split("");
 
   return (
-    <motion.section
-      id="main"
-      className="h-screen flex justify-center"
-      initial="hidden"
-      animate="visible"
-      transition={{ staggerChildren: 0.05 }}
-    >
+    <section id="main" className="h-screen flex justify-center">
       <div className="pt-12 md:pt-28 text-center">
-        <p>Hi there! I am</p>
-        <h1 className="text-dynamic font-extrabold text-primary">
+        <motion.p
+          variants={elementVariants}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 0.05, duration: 0.5 }}
+        >
+          Hi there! I am
+        </motion.p>
+        <motion.h1
+          className="text-dynamic font-extrabold text-primary"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
           {name.map((letter, index) => (
             <motion.span key={index} variants={letterVariants}>
               {letter}
             </motion.span>
           ))}
-        </h1>
-        <p className="md:pt-5 mx-auto w-11/12 text-justify sm:text-center md:w-7/12">
+        </motion.h1>
+        <motion.p
+          className="md:pt-5 mx-auto w-11/12 text-justify sm:text-center md:w-7/12"
+          variants={elementVariants}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 1.3, duration: 0.4 }}
+        >
           An <b>Information Technology</b> student from the Philippines who
           wants to design meaningful systems for a more connected and efficient
           world.
-        </p>
-        <Image
-          src={`/images/hero-img-rounded.png`}
-          alt="Image of Reeman Singh"
-          width={220}
-          height={220}
-          className="pt-6 mx-auto"
-        />
-        <div className="flex flex-col sm:flex-row justify-center gap-2 mt-8 w-3/4 mx-auto">
-          <button
-            className="btn-primary flex items-center justify-center"
-            onClick={handleComingSoon}
-          >
-            <HiDocumentDownload className="mr-1" />
-            <div className="">Download my Resume</div>
-          </button>
-          <a href="#contact">
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 80,
+            delay: 1.45,
+          }}
+        >
+          <Image
+            src={`/images/hero-img-rounded.png`}
+            alt="Image of Reeman Singh"
+            width={220}
+            height={220}
+            className="pt-6 mx-auto"
+          />
+        </motion.div>
+        <motion.div
+          className="flex flex-col sm:flex-row justify-center gap-2 mt-8 w-3/4 mx-auto"
+          variants={elementVariants}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 1.6, duration: 0.5 }}
+        >
+          <a className="flex justify-center">
+            <button
+              className="btn-primary flex items-center justify-center"
+              onClick={handleComingSoon}
+            >
+              <HiDocumentDownload className="mr-1" />
+              <div className="">Download my Resume</div>
+            </button>
+          </a>
+
+          <a href="#contact" className="flex justify-center">
             <button className="btn-primary flex items-center justify-center">
               <AiFillMessage className="mr-1" />
               <div className="">Get in Touch</div>
             </button>
           </a>
-        </div>
+        </motion.div>
       </div>
-    </motion.section>
+    </section>
   );
 }
